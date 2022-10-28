076141d78152347a331bc59eadd467ff544c3220
Harry <harrisinehamric@gmail.com>
2022-10-28 08:58:57 -0500

    push

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import math
 from dateutil.relativedelta import relativedelta
 
-TOKEN = 'MTAzNDg5NDU5NjgxMzA5OTA0OA.GK73OT.q5JqV1NPLoQAQbn0k3040QYpKjjqhGZ4T0emJk'
+TOKEN = 'MTAzNDg5NDU5NjgxMzA5OTA0OA.GLysiq.5ltp0sxNaVX7ZFRpocaatgKXSOKyh_26zL7JZM'
 
 client = discord.Client(intents=discord.Intents.all())
 
@@ -15,28 +15,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    #if '!countdown start' in message.content:
-    if 'a' in message.content:
+    if client.user.mentioned_in(message):
         now = datetime.now()
         print(now)
         mw2Time = datetime.strptime("27/10/2022 23:00:00", "%d/%m/%Y %H:%M:%S")
         print(mw2Time)
         diff = relativedelta(mw2Time, now)
-        firstMessage = '**MW2 IS %d DAY %d HOURS %d MINUTES %d SECONDS AWAY**' % (diff.days, diff.hours, diff.minutes, diff.seconds) 
+        if(diff.days != 0):
+            firstMessage = '**MW2 IS %d DAY %d HOURS %d MINUTES %d SECONDS AWAY**' % (diff.days, diff.hours, diff.minutes, diff.seconds) 
+        else:
+            firstMessage = '**MW2 IS %d HOURS %d MINUTES %d SECONDS AWAY**' % (diff.hours, diff.minutes, diff.seconds) 
         botMessage = await message.channel.send(firstMessage)
-        time.sleep(30)
-        while(True):
-            now = datetime.now()
-            print(now)
-            mw2Time = datetime.strptime("27/10/2022 23:00:00", "%d/%m/%Y %H:%M:%S")
-            print(mw2Time)
-            diff = relativedelta(mw2Time, now)
-            if(diff.days != 0):
-                editMessage = '**MW2 IS %d DAY %d HOURS %d MINUTES %d SECONDS AWAY**' % (diff.days, diff.hours, diff.minutes, diff.seconds) 
-            else:
-                editMessage = '**MW2 IS %d HOURS %d MINUTES %d SECONDS AWAY**' % (diff.hours, diff.minutes, diff.seconds) 
-            await botMessage.edit(content=editMessage)
-            time.sleep(30)
-
 
 client.run(TOKEN)
